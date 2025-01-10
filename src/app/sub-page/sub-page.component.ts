29b6502646cbec68b849975ca63d196e01ee7047
vivekmengade <vivekmengade@gmail.com>
2025-01-10 15:43:57 +0530

    today learned about commands prompt in help section of angular and practice it

---
+++ src/app/sub-page/sub-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sub-page',
+  imports: [],
+  templateUrl: './sub-page.component.html',
+  styleUrl: './sub-page.component.css'
+})
+export class SubPageComponent {
+
+}
---
+++ src/app/user-auth/sign-out/sign-out.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-out',
+  imports: [],
+  templateUrl: './sign-out.component.html',
+  styleUrl: './sign-out.component.css'
+})
+export class SignOutComponent {
+
+}
